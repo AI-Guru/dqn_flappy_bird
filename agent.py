2a14410cccf2f10724be78d4d6a558d86dc42df8
Tristan AI-Guru <tristan@ai-guru.de>
2018-11-20 14:48:10 +0100

    Refactoring. Cleanup. Documenting.

--- agent.py
+++ agent.py
@@ -7,8 +7,29 @@ import matplotlib.pyplot as plt
 
 
 class DQNAgent:
+    """
+    This is the implementation of an DQN-Agent.
+    It allows for doing deep reinforcement learning.
+    It also does minibatch-replay against catastrophic forgetting.
+    """
 
     def __init__(self, name, model, number_of_actions, gamma, final_epsilon, initial_epsilon, number_of_iterations, replay_memory_size, minibatch_size):
+        """
+        Creates a DQN-agent.
+
+        Args:
+            name (string): Name of the agent.
+            model (keras model): A neural network.
+            number_of_actions (int): Number of actions the agent can perform.
+            gamma (float): Weight factor for future rewards.
+            final_epsilon (float): Final value for epsilon-greedy.
+            initial_epsilon (float): Initial value for epsilon-greedy.
+            number_of_iterations (int): How many iterations to run.
+            replay_memory_size (int): Size of the memory for replay.
+            minibatch_size (int): Size of the minibatches used during replay.
+        """
+
+        # Constructor parameters.
         self.name = name
         self.model = model
         self.number_of_actions = number_of_actions
@@ -19,20 +40,30 @@ class DQNAgent:
         self.replay_memory_size = replay_memory_size
         self.minibatch_size = minibatch_size
 
-        # BLA
+        # Additional variables.
         self.start_time = time.time()
         self.replay_memory = []
         self.current_iteration = 0
         self.epsilon_decrements = np.linspace(self.initial_epsilon, self.final_epsilon, self.number_of_iterations)
         self.model_save_frequency = None
 
+        # Additional functions. See below.
         self.track_rewards = False
         self.track_episodes = False
         self.track_maxq = False
-        self.model_saving = False
+        self.save_model = False
 
 
     def enable_rewards_tracking(self, rewards_running_means_length):
+        """
+        Enables tracking the rewards during training.
+
+        Uses a running mean.
+
+        Args:
+            rewards_running_means_length (int): Amount of values used for computing the running mean.
+        """
+
         self.track_rewards = True
         self.rewards_running_means_length = rewards_running_means_length
         self.rewards_array = [0.0] * rewards_running_means_length
@@ -41,6 +72,15 @@ class DQNAgent:
 
 
     def enable_episodes_tracking(self, episodes_running_means_length):
+        """
+        Enables tracking the episodes during training.
+
+        Uses a running mean.
+
+        Args:
+            episodes_running_means_length (int): Amount of values used for computing the running mean.
+        """
+
         self.track_episodes = True
         self.current_episode_length = 0
         self.episodes_running_means_length = episodes_running_means_length
@@ -50,6 +90,15 @@ class DQNAgent:
 
 
     def enable_maxq_tracking(self, maxq_running_means_length):
+        """
+        Enables tracking the max-q-values during training.
+
+        Uses a running mean.
+
+        Args:
+            maxq_running_means_length (int): Amount of values used for computing the running mean.
+        """
+
         self.track_maxq = True
         self.maxq_running_means_length = maxq_running_means_length
         self.maxq_array = [0.0] * maxq_running_means_length
@@ -58,16 +107,43 @@ class DQNAgent:
 
 
     def enable_model_saving(self, model_save_frequency):
-        self.model_saving = True
+        """
+        Enables saving the model during training.
+
+        Args:
+            model_save_frequency (int): Frequency for saving the model automatically.
+        """
+
+        self.save_model = True
         self.model_save_frequency = model_save_frequency
 
 
     def enable_plots_saving(self, plots_save_frequency):
+        """
+        Enables automatic plotting of key-values.
+
+        Args:
+            plots_save_frequency (int): Frequency for plotting the values automatically.
+        """
+
         self.plots_saving = True
         self.plots_save_frequency = plots_save_frequency
 
 
     def get_action(self, state):
+        """
+        Returns an action.
+
+        This implements epsilon greedy. That is, it returns with a certain probability
+        either a random action or a predicted action.
+
+        Args:
+            state (ndarray): A state of the environment.
+
+        Returns:
+            ndarray: A one-hot-encoded action.
+        """
+
         self.current_epsilon = self.epsilon_decrements[self.current_iteration]
 
         prediction = self.model.predict(np.expand_dims(state, axis=0))[0]
@@ -126,33 +202,31 @@ class DQNAgent:
 
     def replay_memory_via_minibatch(self):
 
-        # sample random minibatch
+        # Sample random minibatch and unpack it.
         minibatch = random.sample(self.replay_memory, min(len(self.replay_memory), self.minibatch_size))
-
-        # unpack minibatch
         state_batch = np.array([d[0] for d in minibatch])
         action_batch = np.array([d[1] for d in minibatch])
         reward_batch = np.array([d[2] for d in minibatch])
         state_next_batch = np.array([d[3] for d in minibatch])
         terminal_batch = np.array([d[4] for d in minibatch])
 
-        # Do gradient descent.
-        q = self.model.predict(state_batch)
-        q_next = self.model.predict(state_next_batch)
+        # Prepare q-values.
+        q_values = self.model.predict(state_batch)
+        q_values_next = self.model.predict(state_next_batch)
         for i in range(len(minibatch)):
             if terminal_batch[i] == False:
-                q[i, np.argmax(action_batch[i])] = reward_batch[i] + self.gamma * np.max(q_next[i])
+                q_values[i, np.argmax(action_batch[i])] = reward_batch[i] + self.gamma * np.max(q_values_next[i])
             else:
-                q[i, np.argmax(action_batch[i])] = reward_batch[i]
+                q_values[i, np.argmax(action_batch[i])] = reward_batch[i]
 
-        #targets[:, np.argmax(action_batch, axis=1)] = reward_batch + agent.gamma * np.max(Q_sa, axis=1) * np.invert(terminal_batch)
-        self.model.train_on_batch(state_batch, q)
+        # Do gradient descent.
+        self.model.train_on_batch(state_batch, q_values)
 
         # Save the model if configured.
-        if self.model_saving == True:
+        if self.save_model == True:
             self.save_model_if_enabled()
 
-        # Save the model if configured.
+        # Save the plots if configured.
         if self.plots_saving == True:
             self.save_plots_if_enabled()
 
@@ -161,6 +235,10 @@ class DQNAgent:
         # Saving the model wrt. frequency or on last iteration.
         if self.current_iteration % self.model_save_frequency == 0 or self.current_iteration == self.number_of_iterations - 1:
             self.save_model("{}-model-{:08d}.h5".format(self.name, self.current_iteration + 1))
+
+
+    def save_model(self, path):
+        self.model.save(path)
 
 
     def save_plots_if_enabled(self):
@@ -183,8 +261,6 @@ class DQNAgent:
                 plt.close()
 
 
-    def save_model(self, path):
-        self.model.save(path)
 
 
     def predict_on_state(self, state):
@@ -227,31 +303,39 @@ class DDQNAgent(DQNAgent):
 
     def replay_memory_via_minibatch(self):
 
-        # sample random minibatch
+        # Sample random minibatch and unpack it.
         minibatch = random.sample(self.replay_memory, min(len(self.replay_memory), self.minibatch_size))
-
-        # unpack minibatch
         state_batch = np.array([d[0] for d in minibatch])
         action_batch = np.array([d[1] for d in minibatch])
         reward_batch = np.array([d[2] for d in minibatch])
         state_next_batch = np.array([d[3] for d in minibatch])
         terminal_batch = np.array([d[4] for d in minibatch])
 
-        # Do gradient descent.
-        targets = self.target_model.predict(state_batch)
-        Q_sa = self.target_model.predict(state_next_batch)
+        # Prepare q-values.
+        q_values = self.model.predict(state_batch)
+        q_values_next = self.model.predict(state_next_batch)
+        q_values_next_target = self.target_model.predict(state_next_batch)
         for i in range(len(minibatch)):
             if terminal_batch[i] == False:
-                targets[i, np.argmax(action_batch[i])] = reward_batch[i] + self.gamma * np.max(Q_sa[i])
+                argmax = np.argmax(q_values_next[i])
+                q_values[i, np.argmax(action_batch[i])] = reward_batch[i] + self.gamma * q_values_next_target[i][argmax]
             else:
-                targets[i, np.argmax(action_batch[i])] = reward_batch[i]
+                q_values[i, np.argmax(action_batch[i])] = reward_batch[i]
 
-        #targets[:, np.argmax(action_batch, axis=1)] = reward_batch + agent.gamma * np.max(Q_sa, axis=1) * np.invert(terminal_batch)
-        self.model.train_on_batch(state_batch, targets)
+        # Do gradient descent.
+        self.model.train_on_batch(state_batch, q_values)
 
         # Copy weights to target net.
         if self.number_of_iterations % self.model_copy_interval == 0:
             self.target_model.set_weights(self.model.get_weights())
+
+        # Save the model if configured.
+        if self.save_model == True:
+            self.save_model_if_enabled()
+
+        # Save the plots if configured.
+        if self.plots_saving == True:
+            self.save_plots_if_enabled()
 
 
     def save_model(self, path):
--- train-cartpole.py
+++ train-cartpole.py
@@ -7,7 +7,7 @@ import numpy as np
 import gym
 import sys
 import matplotlib.pyplot as plt
-from agent import DQNAgent
+from agent import DQNAgent, DDQNAgent
 from keras import models, layers, optimizers, initializers
 
 
@@ -18,17 +18,32 @@ def main():
     model.summary()
 
     print("Creating agent...")
-    agent = DQNAgent(
-        name="cartpole",
-        model=model,
-        number_of_actions=2,
-        gamma=0.95,
-        final_epsilon=0.01,
-        initial_epsilon=1.0,
-        number_of_iterations=1000000,
-        replay_memory_size=2000,
-        minibatch_size=32
-    )
+    agent_type = "ddqn"
+    if agent_type == "dqn":
+        agent = DQNAgent(
+            name="cartpole-dqn",
+            model=model,
+            number_of_actions=2,
+            gamma=0.95,
+            final_epsilon=0.01,
+            initial_epsilon=1.0,
+            number_of_iterations=1000000,
+            replay_memory_size=2000,
+            minibatch_size=32
+        )
+    elif agent_type == "ddqn":
+        agent = DDQNAgent(
+            name="cartpole-ddqn",
+            model=model,
+            number_of_actions=2,
+            gamma=0.95,
+            final_epsilon=0.01,
+            initial_epsilon=1.0,
+            number_of_iterations=1000000,
+            replay_memory_size=2000,
+            minibatch_size=32,
+            model_copy_interval=100
+        )
     agent.enable_rewards_tracking(rewards_running_means_length=100)
     agent.enable_episodes_tracking(episodes_running_means_length=100)
     agent.enable_maxq_tracking(maxq_running_means_length=100)
